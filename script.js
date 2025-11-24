41bc9d1bb9e0cb22d841ac1067acacd526bf9155
Jean Decian <jeandecian@hotmail.fr>
2025-11-23 20:49:40 -0500

    add personal name

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const data = {
+  personal: { name: "Jean Decian" },
+};
+
 function createElement(tag, classes, content = "") {
   const element = document.createElement(tag);
 
@@ -10,6 +14,16 @@ function createElement(tag, classes, content = "") {
 function renderResumeCV() {
   const container = document.getElementById("resume-cv-container");
   if (!container) return;
+
+  const header = createElement("header", "pb-4 mb-6 border-b border-gray-200");
+  const nameEl = createElement(
+    "h1",
+    "text-4xl font-extrabold text-blue-800 tracking-tighter",
+    data.personal.name
+  );
+  header.appendChild(nameEl);
+
+  container.appendChild(header);
 }
 
 document.addEventListener("DOMContentLoaded", renderResumeCV);
